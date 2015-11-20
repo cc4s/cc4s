4cd697a6c187de6a4948c8be29f0d77f0f04a12e
Felix Hummel <f.hummel@fkf.mpg.de>
2015-11-20 10:18:41 +0100

    missing MathFunctions added.

---
+++ src/util/MathFunctions.cxx
@@ -0,0 +1,13 @@
+#include "MathFunctions.hpp"
+#include <cmath>
+
+// univariate functions
+double MathFunctions::sqrt(double const x) {
+  return std::sqrt(x);
+}
+
+// bivariate functions
+double MathFunctions::divide(double const x, double const y) {
+  return x / y;
+}
+
---
+++ src/util/MathFunctions.hpp
@@ -0,0 +1,14 @@
+#ifndef MATH_FUNCTIONS_DEFINED
+#define MATH_FUNCTIONS_DEFINED
+
+class MathFunctions {
+public:
+  // univariate functions
+  static double sqrt(double const x);
+
+  // bivariate functions
+  static double divide(double const x, double const y);
+};
+
+#endif
+
