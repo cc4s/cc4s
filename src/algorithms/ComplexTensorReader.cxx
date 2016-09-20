d3f1db8f4edfaf3f59a7622106c1e80fefee3b00
Felix Hummel <f.hummel@fkf.mpg.de>
2016-09-20 16:18:31 +0200

    ComplexTensorReader added.

---
+++ src/algorithms/ComplexTensorReader.cxx
@@ -0,0 +1,44 @@
+/*Copyright (c) 2016, Andreas Grueneis and Felix Hummel, all rights reserved.*/
+#include <algorithms/ComplexTensorReader.hpp>
+#include <math/Complex.hpp>
+#include <util/TensorIo.hpp>
+#include <util/Log.hpp>
+#include <Cc4s.hpp>
+#include <fstream>
+#include <ctf.hpp>
+
+using namespace CTF;
+using namespace cc4s;
+
+ALGORITHM_REGISTRAR_DEFINITION(ComplexTensorReader);
+
+ComplexTensorReader::ComplexTensorReader(
+  std::vector<Argument> const &argumentList
+): Algorithm(argumentList) {
+}
+
+ComplexTensorReader::~ComplexTensorReader() {
+}
+
+void ComplexTensorReader::run() {
+  std::string dataName(getArgumentData("Data")->getName());
+
+  // make sure all processes start reading the file at the same time in case
+  // it has been modified before
+  MPI_Barrier(Cc4s::world->comm);
+
+  std::string mode(getTextArgument("mode", "text"));
+  Tensor<complex> *A;
+  if (mode == "binary") {
+    std::string fileName(getTextArgument("file", dataName + ".bin"));
+    A = TensorIo::readBinary<complex>(fileName);
+  } else {
+    std::string fileName(getTextArgument("file", dataName + ".dat").c_str());
+    std::string delimiter(getTextArgument("delimiter", " "));
+    int64_t bufferSize(getIntegerArgument("bufferSize", 128l*1024*1024));
+    A = TensorIo::readText<complex>(fileName, delimiter, bufferSize);
+  }
+  A->set_name(dataName.c_str());
+  allocatedTensorArgument<complex>("Data", A);
+}
+
