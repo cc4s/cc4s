74def47dfdfed28b3fb2b026b6242b63a99fe21d
Felix Hummel <f.hummel@fkf.mpg.de>
2016-09-23 11:45:56 +0200

    Linking test written

---
+++ src/math/SingularValueDecomposition.hpp
@@ -0,0 +1,32 @@
+#ifndef SINGULAR_VALUE_DECOMPOSITION_DEFINED
+#define SINGULAR_VALUE_DECOMPOSITION_DEFINED
+
+#include <math/Complex.hpp>
+#include <util/DryTensor.hpp>
+#include <ctf.hpp>
+#include <random>
+
+namespace cc4s {
+  template <typename F>
+  class SingularValueDecomposition {
+  public:
+    SingularValueDecomposition(CTF::Matrix<F> const &matrix);
+    CTF::Matrix<F> &get();
+
+  protected:
+    CTF::Matrix<F> inverse;
+  };
+
+  template <typename F>
+  class DrySingularValueDecomposition {
+  public:
+    DrySingularValueDecomposition(DryMatrix<F> const &matrix);
+    DryMatrix<F> &get();
+
+  protected:
+    DryMatrix<F> inverse;
+  };
+}
+
+#endif
+
