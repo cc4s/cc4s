bd7d67f09386a37a7e75cccf6559c29de1a5ccb5
ke liao <you@example.com>
2016-09-28 12:23:39 +0200

    Structure factor started

--- src/algorithms/FiniteSizeCorrection.cxx
+++ src/algorithms/FiniteSizeCorrection.cxx
@@ -1,6 +1,7 @@
 #include <algorithms/FiniteSizeCorrection.hpp>
 #include <math/Complex.hpp>
 #include <math/ComplexTensor.hpp>
+#include <math/MathFunctions.hpp>
 #include <util/DryTensor.hpp>
 #include <util/Log.hpp>
 #include <util/Exception.hpp>
@@ -25,18 +26,34 @@ void FiniteSizeCorrection::run() {
     getTensorArgument<complex>("ParticleHoleCoulombVertex")
   );
   Tensor<> *VG(getTensorArgument<>("CoulombKernel"));
+  Tensor<> invVG(false, *VG);
   {
-    Tensor<complex> CGai(GammaGai);
     class InvSqrt {
     public:
       double operator ()(double x) {
-        LOG_RANK(1, "FiniteSizeCorrection") << "invSqrt of " << std::isinf(x) << std::endl;
-        return std::isinf(x) ? 0.0 : std::sqrt(1.0 / x);
+//        LOG_RANK(1, "FiniteSizeCorrection") << "is inf? " << std::isinf(x) <<"invert of "<< std::sqrt(1.0/x) << std::endl;
+        return std::sqrt(1.0 / x);
       }
     };
     InvSqrt invSqrt;
     Univar_Function<> fInvSqrt(invSqrt);
-    VG->sum(1.0,*VG,"G", 0.0,"G", fInvSqrt);
+    invVG.sum(1.0,*VG,"G", 0.0,"G", fInvSqrt);
+    Tensor<complex> CGai(*GammaGai);
+    CGai["Gai"] *= invVG["G"];
+
+    Tensor<complex> conjCGai(false, CGai);
+    Univar_Function<complex> fConj(conj<complex>);
+    conjCGai.sum(1.0,CGai,"Gai", 0.0,"Gai", fConj);
+/*
+    Tensor<> realCGai(3, CGai.lens, CGai.sym, *CGai.wrld, "realCGai");
+    Tensor<> imagCGai(3, CGai.lens, CGai.sym, *CGai.wrld, "realCGai");
+    fromComplexTensor(CGai, realCGai, imagCGai);
+*/
+    Tensor<> *Tabij(getTensorArgument("DoublesAmplitudes"));
+    int NG(CGai.lens[0]);
+    Vector<> SG(NG, *CGai.wrld, "SG");
+    // SG = Tabij ( 2*conj(CGai)*CGbj - conj(CGaj)*CGbi )
+    SG["G"] = conjCGai["Gai"] * CGai["Gbj"] * (*Tabij)["abij"];
   }
   
   double energyCorrection(0.0);
