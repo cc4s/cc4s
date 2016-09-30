55538dcf6a0f9730c156625eb07b09f6bcbf8e3d
Felix Hummel <f.hummel@fkf.mpg.de>
2016-09-30 18:36:42 +0200

    example code added.

--- src/math/SingularValueDecomposition.cxx
+++ src/math/SingularValueDecomposition.cxx
@@ -103,6 +103,79 @@ template
 DryMatrix<complex> &DrySingularValueDecomposition<complex>::get();
 
 /*
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "mpi.h"
+
+#define AA(i,j) AA[(i)*M+(j)]
+
+
+int main(int argc, char **argv) {
+   int i, j, k;
+   int myrank_mpi, nprocs_mpi;
+
+   MPI_Init( &argc, &argv);
+   MPI_Comm_rank(MPI_COMM_WORLD, &myrank_mpi);
+   MPI_Comm_size(MPI_COMM_WORLD, &nprocs_mpi);
+
+   int ictxt, nprow, npcol, myrow, mycol,nb;
+   int info,itemp;
+   int ZERO=0,ONE=1;
+   nprow = 2; npcol = 2; nb =2;
+   Cblacs_pinfo( &myrank_mpi, &nprocs_mpi ) ;
+   Cblacs_get( -1, 0, &ictxt );
+   Cblacs_gridinit( &ictxt, "Row", nprow, npcol );
+   Cblacs_gridinfo( ictxt, &nprow, &npcol, &myrow, &mycol );
+   int M=5;
+   double *AA = (double*) malloc(M*M*sizeof(double));
+   for(i=0;i<M;i++ )
+     for(j=0;j<M;j++)
+        AA[i*M+j]=(2*i+3*j+1);
+
+   double *X = (double*) malloc(M*sizeof(double));
+   X[0]=1;X[1]=1;X[2]=0;X[3]=0;X[4]=1;
+
+   int descA[9],descx[9],descy[9];
+   int mA = numroc_( &M, &nb, &myrow, &ZERO, &nprow );
+   int nA = numroc_( &M, &nb, &mycol, &ZERO, &npcol );
+   int nx = numroc_( &M, &nb, &myrow, &ZERO, &nprow );
+   int my = numroc_( &M, &nb, &myrow, &ZERO, &nprow );
+   descinit_(descA, &M,   &M,   &nb,  &nb,  &ZERO, &ZERO, &ictxt, &mA,  &info);
+   descinit_(descx, &M, &ONE,   &nb, &ONE,  &ZERO, &ZERO, &ictxt, &nx, &info);
+
+   descinit_(descy, &M, &ONE,   &nb, &ONE,  &ZERO, &ZERO, &ictxt, &my, &info);
+   double *x = (double*) malloc(nx*sizeof(double));
+   double *y = (double*) calloc(my,sizeof(double));
+   double *A = (double*) malloc(mA*nA*sizeof(double));
+   int sat,sut;
+   for(i=0;i<mA;i++)
+
+   for(j=0;j<nA;j++){
+                sat= (myrow*nb)+i+(i/nb)*nb;
+                sut= (mycol*nb)+j+(j/nb)*nb;
+                A[j*mA+i]=AA(sat,sut);
+        }
+
+
+   for(i=0;i<nx;i++){
+                sut= (myrow*nb)+i+(i/nb)*nb;
+                x[i]=X[sut];
+        }
+
+   double alpha = 1.0; double beta = 0.0;
+   pdgemv_("N",&M,&M,&alpha,A,&ONE,&ONE,descA,x,&ONE,&ONE,descx,&ONE,&beta,y,&ONE,&ONE,descy,&ONE);
+
+   Cblacs_barrier(ictxt,"A");
+   for(i=0;i<my;i++)
+   printf("rank=%d %.2f \n", myrank_mpi,y[i]);
+   Cblacs_gridexit( 0 );
+   MPI_Finalize();
+   return 0;
+}
+
+
+
 #include <mpi.h>
 #include <iostream>
 #include <vector>
