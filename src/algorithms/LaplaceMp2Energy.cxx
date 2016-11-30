3c8e6f7ad870c6b19929a0cc6a8ebf8a3ddd6c32
Theo Tsatsoulis <T.Tsatsoulis@fkf.mpg.de>
2016-11-30 10:57:09 +0100

    Added initial files for LaplaceMp2Energy

---
+++ src/algorithms/LaplaceMp2Energy.cxx
@@ -0,0 +1,76 @@
+#include <algorithms/LaplceMp2Energy.hpp>
+#include <math/MathFunctions.hpp>
+#include <tcc/DryTensor.hpp>
+#include <util/Log.hpp>
+#include <util/Exception.hpp>
+#include <Cc4s.hpp>
+#include <ctf.hpp>
+
+using namespace CTF;
+using namespace cc4s;
+
+ALGORITHM_REGISTRAR_DEFINITION(LaplceMp2Energy);
+
+LaplceMp2Energy::LaplceMp2Energy(
+  std::vector<Argument> const &argumentList
+): Algorithm(argumentList) {
+}
+
+LaplceMp2Energy::~LaplceMp2Energy() {
+}
+
+void LaplceMp2Energy::run() {
+  Tensor<> *epsi(getTensorArgument("HoleEigenEnergies"));
+  Tensor<> *epsa(getTensorArgument("ParticleEigenEnergies"));
+  Tensor<> *Vabij(getTensorArgument("PPHHCoulombIntegrals"));
+ 
+  Tensor<> Tabij(false, Vabij);
+  Tabij["abij"] =  (*epsi)["i"];
+  Tabij["abij"] += (*epsi)["j"];
+  Tabij["abij"] -= (*epsa)["a"];
+  Tabij["abij"] -= (*epsa)["b"];
+
+  Bivar_Function<> fDivide(&divide<double>);
+  Tabij.contract(1.0, (*Vabij),"abij", Tabij,"abij", 0.0,"abij", fDivide);
+
+  Scalar<> energy(*Cc4s::world);
+  double e, dire, exce;
+
+  energy[""] = 2.0 * Tabij["abij"] * (*Vabij)["abij"];
+  dire = energy.get_val();
+  energy[""] = Tabij["abji"] * (*Vabij)["abij"];
+  exce = -1.0 * energy.get_val();
+  e = dire + exce;
+
+  LOG(0, "MP2") << "e=" << e << std::endl;
+  LOG(1, "MP2") << "MP2d=" << dire << std::endl;
+  LOG(1, "MP2") << "MP2x=" << exce << std::endl;
+
+  setRealArgument("Mp2Energy", e);
+}
+
+void LaplceMp2Energy::dryRun() {
+  //DryTensor<> *Vabij(
+  getTensorArgument<double, DryTensor<double>>("PPHHCoulombIntegrals");
+  //);
+
+  // Read the Particle/Hole Eigenenergies epsi epsa required for the energy
+  DryTensor<> *epsi(
+    getTensorArgument<double, DryTensor<double>>("HoleEigenEnergies")
+  );
+  DryTensor<> *epsa(
+    getTensorArgument<double, DryTensor<double>>("ParticleEigenEnergies")
+  );
+  
+  // Compute the No,Nv
+  int No(epsi->lens[0]);
+  int Nv(epsa->lens[0]);
+
+  // Allocate the doubles amplitudes
+  int syms[] = { NS, NS, NS, NS };
+  int vvoo[] = { Nv, Nv, No, No };
+  DryTensor<> Tabij(4, vvoo, syms);
+
+  DryScalar<> energy();
+}
+
---
+++ src/algorithms/LaplaceMp2Energy.hpp
@@ -0,0 +1,28 @@
+/*Copyright (c) 2015, Andreas Grueneis and Felix Hummel, all rights reserved.*/
+#ifndef LAPLACE_MP2_ENERGY_DEFINED
+#define LAPLACE_MP2_ENERGY_DEFINED
+
+#include <algorithms/Algorithm.hpp>
+
+namespace cc4s {
+  /**
+   * \brief Caclulates MP2 energy from the Coulomb Integrals \f$V_{ij}^{ab}.
+   */
+  class LaplaceMp2Energy: public Algorithm {
+  public:
+    ALGORITHM_REGISTRAR_DECLARATION(LaplaceMp2Energy);
+    LaplaceMp2Energy(std::vector<Argument> const &argumentList);
+    virtual ~LaplaceMp2Energy();
+    /**
+     * \brief Calculates MP2 energy from Coulomb integrals Vabij
+     */
+    virtual void run();
+    /**
+     * \brief Dry run for the MP2 energy from Coulomb integrals Vabij
+     */
+    virtual void dryRun();
+  };
+}
+
+#endif
+
