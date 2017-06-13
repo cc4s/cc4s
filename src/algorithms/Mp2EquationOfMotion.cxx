a053bd2e14d3e6932c6ac1758e6e74488e78442a
Alejandro Gallo <aamsgallo@gmail.com>
2017-06-13 15:59:33 +0200

    Fix pointer problem and write out bug

--- src/algorithms/Mp2EquationOfMotion.cxx
+++ src/algorithms/Mp2EquationOfMotion.cxx
@@ -49,26 +49,25 @@ void Mp2EquationOfMotion::run() {
   Tabij["abij"] -= (*epsa)["a"];
   Tabij["abij"] -= (*epsa)["b"];
 
-  T Dabij(false, Vabij);
-  Dabij["abij"] = Tabij["abij"];
-
+  LOG(1, "MP2_EOM") <<
+    "Creating doubles amplitudes" << totalDimension << std::endl;
   CTF::Bivar_Function<> fDivide(&divide<double>);
   Tabij.contract(1.0, (*Vabij),"abij", Tabij,"abij", 0.0,"abij", fDivide);
 
-  // Initialize 2 body L and R
+  // Create L and R
   int oneBodyLens[] = {Nv, No};
   int oneBodySyms[] = {NS, NS};
+  T *Lai( new T(2, oneBodyLens, oneBodySyms, *Cc4s::world, "Lai") );
   T Labij(false, Vabij);
-  T Lai( new CTF::Tensor<>(2, oneBodyLens, oneBodySyms, *Cc4s::world, "Lai") );
+  T *Rai( new T(2, oneBodyLens, oneBodySyms, *Cc4s::world, "Rai") );
   T Rabij(false, Vabij);
-  T Rai( new CTF::Tensor<>(2, oneBodyLens, oneBodySyms, *Cc4s::world, "Rai") );
 
   // kinetic terms
   int kineticLensVirtual[] = {Nv, Nv};
   int kineticSyms[] = {NS, NS};
-  T Fab( new CTF::Tensor<>(2, kineticLensVirtual, kineticSyms, *Cc4s::world, "Fab") );
+  T Fab( new T(2, kineticLensVirtual, kineticSyms, *Cc4s::world, "Fab") );
   int kineticLensOccupied[] = {No, No};
-  T Fij( new CTF::Tensor<>(2, kineticLensOccupied, kineticSyms, *Cc4s::world, "Fij") );
+  T Fij( new T(2, kineticLensOccupied, kineticSyms, *Cc4s::world, "Fij") );
 
   Fab["aa"] = (*epsa)["a"];
   Fij["ii"] = (*epsi)["i"];
@@ -78,16 +77,19 @@ void Mp2EquationOfMotion::run() {
   //and doubles excitations
   int hLens[] = {totalDimension-1, totalDimension-1};
   int hSyms[] = {NS, NS};
-  T *Hpq( new CTF::Tensor<>(2, hLens, hSyms, *Cc4s::world, "Lai") );
+  int64_t hIndices[1];
+  double hValues[1];
+  T *Hpq( new CTF::Tensor<>(2, hLens, hSyms, *Cc4s::world, "Hpq") );
 
   CTF::Scalar<> energy(0.0);
+  double energy_val(0.0);
 
   for (int64_t i = 0 ; i < totalDimension-1 ; i++) {
-    getCanonicalPerturbationBasis(Lai, Labij, i);
+    getCanonicalPerturbationBasis(*Lai, Labij, i);
     for (int64_t j = 0 ; j < totalDimension-1; j++) {
-      getCanonicalPerturbationBasis(Rai, Rabij, i);
-      energy[""]  = ( + 0.5 ) * Lai["ib"] *  (*Vabij)["klie"] * Rabij["ebkl"];
-      energy[""] += ( + 0.5 ) * Lai["ib"] *  (*Vabij)["kbde"] * Rabij["deki"];
+      getCanonicalPerturbationBasis(*Rai, Rabij, i);
+      energy[""]  = ( + 0.5 ) * (*Lai)["ib"] *  (*Vabij)["klie"] * Rabij["ebkl"];
+      energy[""] += ( + 0.5 ) * (*Lai)["ib"] *  (*Vabij)["kbde"] * Rabij["deki"];
       energy[""] += ( - 1.0 ) * Labij ["ijcd"] * Fij["mi"] * Rabij["cdmj"];
       energy[""] += ( + 1.0 ) * Labij ["ijcd"] * Fij["mj"] * Rabij["cdmi"];
       energy[""] += ( - 1.0 ) * Labij["ijdc"] * Fab["de"] * Rabij["ecij"];
@@ -112,28 +114,15 @@ void Mp2EquationOfMotion::run() {
       energy[""] += ( + 0.5 )  * Labij["ijcd"] * (*Vabij)["cdef"] * Rabij["efij"];
       energy[""] += ( + 0.25 ) * Labij["ijcd"] * Tabij["efij"] * (*Vabij)["opef"] * Rabij["cdop"];
       energy[""] += ( + 0.25 ) * Labij["ijcd"] * Tabij["cdmn"] * (*Vabij)["mngh"] * Rabij["ghij"];
+      energy_val = energy.get_val();
+      hValues[0] = energy_val;
+      hIndices[0] = i + j * (totalDimension - 1);
+      (*Hpq).write(1, hIndices, hValues);
+      LOG(1, "MP2_EOM") << "< " << i << " |H| " <<  j << " >"  << " = " << energy_val << std::endl;
     }
   }
 
-/*
- *  auto H20(new CTF::Tensor<>(*Vabij));
- *  auto Habij(new CTF::Tensor<>(*Vabij));
- *
- *  (*H20)["abij"] -= (*Vabij)["abji"];
- *
- *  // cdkl are row indices and abij are column indices
- *  int lens[] = {Nv,Nv,No,No, Nv,Nv,No,No};
- *  int syms[] = {NS,NS,NS,NS, NS,NS,NS,NS};
- *  auto H22( new CTF::Tensor<>(8, lens, syms, *Cc4s::world, "H22cdklabij") );
- *  // diagonal elements
- *  (*H22)["abijabij"] = Dabij["abij"];
- *  (*H22)["cbkjabij"] += (*Habij)["acik"];
- *  (*H22)["adilabij"] += (*Habij)["bdjl"];
- *
- *  allocatedTensorArgument("SimlarityTransformedHamiltonian22", H22);
- *  allocatedTensorArgument("SimlarityTransformedHamiltonian20", H20);
- *  allocatedTensorArgument("EnergyDenominators", new CTF::Tensor<>(Dabij));
- */
+  allocatedTensorArgument("SimlarityTransformedHamiltonianSD", Hpq);
 
 }
 
@@ -156,8 +145,6 @@ void Mp2EquationOfMotion::getCanonicalPerturbationBasis(
     indices[0] -= oneBodyLength;
     Tabij.write(1, indices, values);
   }
-  //Tai.print();
-  //Tabij.print();
 
 }
 
