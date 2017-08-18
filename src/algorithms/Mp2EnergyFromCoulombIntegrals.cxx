e283c03300d26b11d9cc5a0008beba4dad7b766d
Theodoros Tsatsoulis <T.Tsatsoulis@fkf.mpg.de>
2017-08-18 10:52:40 +0200

    add Mp2DoublesAmplitudes as an optional argument

--- src/algorithms/Mp2EnergyFromCoulombIntegrals.cxx
+++ src/algorithms/Mp2EnergyFromCoulombIntegrals.cxx
@@ -6,6 +6,7 @@
 #include <util/Exception.hpp>
 #include <Cc4s.hpp>
 #include <ctf.hpp>
+#include <Options.hpp>
 
 using namespace CTF;
 using namespace cc4s;
@@ -72,12 +73,13 @@ F Mp2EnergyFromCoulombIntegrals::calculateMp2Energy(CTF::Tensor<F> &Vabij) {
   toComplexTensor(*epsa, Fepsa);
 
   // create excitation energy
-  Tensor<F> Tabij(false, Vabij);
-  Tabij.set_name("Tabij");
-  Tabij["abij"] =  Fepsi["i"];
-  Tabij["abij"] += Fepsi["j"];
-  Tabij["abij"] -= Fepsa["a"];
-  Tabij["abij"] -= Fepsa["b"];
+  auto Tabij(new Tensor<F>(Vabij));
+  //  Tensor<F> Tabij(false, Vabij);
+  Tabij->set_name("Tabij");
+  (*Tabij)["abij"] =  Fepsi["i"];
+  (*Tabij)["abij"] += Fepsi["j"];
+  (*Tabij)["abij"] -= Fepsa["a"];
+  (*Tabij)["abij"] -= Fepsa["b"];
 
   // use transform to divide Vabij by Tabij and store in Tabij
   CTF::Transform<F, F>(
@@ -87,7 +89,7 @@ F Mp2EnergyFromCoulombIntegrals::calculateMp2Energy(CTF::Tensor<F> &Vabij) {
       }
     )
   ) (
-    Vabij["abij"], Tabij["abij"]
+     Vabij["abij"], (*Tabij)["abij"]
   );
 
 // TODO: below method requires less memory but does not work with current CTF
@@ -112,13 +114,20 @@ F Mp2EnergyFromCoulombIntegrals::calculateMp2Energy(CTF::Tensor<F> &Vabij) {
   Scalar<F> energy(*Cc4s::world);
   F dire, exce;
 
-  energy[""] = 2.0 * Tabij["abij"] * Vabij["abij"];
+  energy[""] = 2.0 * (*Tabij)["abij"] * Vabij["abij"];
   dire = energy.get_val();
-  energy[""] = Tabij["abji"] * Vabij["abij"];
+  energy[""] = (*Tabij)["abji"] * Vabij["abij"];
   exce = -1.0 * energy.get_val();
   LOG(0, "MP2") << "e=" << (dire + exce) << std::endl;
   LOG(1, "MP2") << "MP2d=" << dire << std::endl;
   LOG(1, "MP2") << "MP2x=" << exce << std::endl;
+
+  if (isArgumentGiven("Mp2DoublesAmplitudes")) {
+    allocatedTensorArgument<F>("Mp2DoublesAmplitudes", Tabij);
+  } else {
+    delete Tabij;
+  }
+
   return dire + exce;
 }
 
