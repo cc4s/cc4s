e33c72c78f7ba16d40381e246eb51b918560299c
Felix Hummel <felix.hummel@tuwien.ac.at>
2017-10-06 14:39:05 +0200

    fix basic cc4s doxygen info

--- src/Cc4s.hpp
+++ src/Cc4s.hpp
@@ -40,12 +40,12 @@ namespace cc4s {
  * currently interfaced to the Vienna Ab-inition Simulation Package.
  * 
  * The software is available on
- * https://vault.hulldamage.org/cc4s.org/doku.php and maybe obtained
+ * https://hulldamage.org/cc4s.org/doku.php and maybe obtained
  * via the command 
  *
- * git clone username@hulldamage.org:/var/www/git.hulldamage.org/repos/cc4s.git/
+ * git clone git@gitlab.com:grueneis/cc4s.git
  *
- * For information or access to the repository please contact <mailto:F.Hummel@fkf.mpg.de>
+ * For information or access to the repository please contact <mailto:andreas.grueneis@tuwien.ac.at>
  *
  * CC4S depends on the CTF and requires MPI to be built as the main parallel execution and communication mechanism.
  *
