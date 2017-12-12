bfd408783351d14858e8674f41e8c803c86d1ca0
Felix Hummel <felix.hummel@tuwien.ac.at>
2017-12-12 17:33:02 +0100

    conjugate gradient started

--- src/algorithms/RpaApxEnergy.cxx
+++ src/algorithms/RpaApxEnergy.cxx
@@ -25,13 +25,53 @@ RpaApxEnergy::~RpaApxEnergy() {
 }
 
 namespace cc4s {
+  // provides vector space structure for imaginary frequency grids
+  class FrequencyGrid {
+  public:
+    FrequencyGrid(const int N): nus(N), ws(N) {
+    }
+    FrequencyGrid(const FrequencyGrid &g): nus(g.nus), ws(g.ws) {
+    }
+    FrequencyGrid &operator =(const FrequencyGrid &g) {
+      for (size_t n(0); n < nus.size(); ++n) {
+        nus[n] = g.nus[n];
+        ws[n] = g.ws[n];
+      }
+      return *this;
+    }
+    FrequencyGrid &operator +=(const FrequencyGrid &g) {
+      for (size_t n(0); n < nus.size(); ++n) {
+        nus[n] += g.nus[n];
+        ws[n] += g.ws[n];
+      }
+      return *this;
+    }
+    FrequencyGrid &operator *=(double s) {
+      for (size_t n(0); n < nus.size(); ++n) {
+        nus[n] *= s;
+        ws[n] *= s;
+      }
+      return *this;
+    }
+    double dot(const FrequencyGrid &g) {
+      double result(0.0);
+      for (size_t n(0); n < nus.size(); ++n) {
+        result += nus[n] * g.nus[n];
+        result += ws[n] * g.ws[n];
+      }
+      return result;
+    }
+    std::vector<double> nus, ws;
+  };
+
   class FrequencyGridOptimizer {
   public:
     FrequencyGridOptimizer(
       const int N, CTF::Tensor<double> epsi, CTF::Tensor<double> epsa
     ):
-      ws(N), nus(N), dws(N), dnus(N)
+      grid(N)
     {
+      LOG(1, "RPA") << "initializing grid optimization" << std::endl;
       int No(epsi.lens[0]);
       int Nv(epsa.lens[0]);
       Dai = new CTF::Tensor<double>(2, std::vector<int>({Nv,No}).data());
@@ -43,10 +83,87 @@ namespace cc4s {
       Dai->read_all(deltas.data(), true);
       std::sort(deltas.begin(), deltas.end());
       double lastNu(0);
-      for (size_t n(0); n < ws.size(); ++n) {
-        nus[n] = deltas[No*Nv*n/N + No*Nv/(2*N)];
-        ws[n] = nus[n] - lastNu;
-        lastNu = nus[n];
+      for (size_t n(0); n < grid.ws.size(); ++n) {
+        grid.nus[n] = deltas[No*Nv*n/N + No*Nv/(2*N)];
+        grid.ws[n] = grid.nus[n] - lastNu;
+        lastNu = grid.nus[n];
+      }
+      Eai = new CTF::Tensor<double>(false, *Dai);
+    }
+
+    void optimize(const int stepCount, const double stepSize) {
+      LOG(1, "RPA") << "optimizing grid" << std::endl;
+      NEW_FILE("E.dat") << getError(grid) << std::endl;
+      for (int m(0); m < stepCount; ++m) {
+        auto direction(getGradient(grid));
+        direction *= -1.0;
+        FILE("E.dat") << lineSearch(grid, direction) << std::endl;
+//        LOG(1, "RPA") << lineSearch(grid, direction) << std::endl;
+      }
+    }
+
+    double lineSearch(FrequencyGrid &grid, const FrequencyGrid &direction) {
+      LOG(1, "RPA") << "line search" << std::endl;
+      // starting vectors
+      auto g0(grid);
+      grid += direction;
+      double E0(getError(g0));
+      double E1(getError(grid));
+      double Em;
+      double alpham;
+      double alpha1(1.0);
+      // increase alpha until error also increases
+      while (E1 <= E0) {
+        alpham = alpha1;
+        alpha1 *= 2.0;
+        grid = g0;
+        auto dg(direction);
+        grid += dg *= alpha1;
+        Em = E1;
+        E1 = getError(grid);
+        LOG(1, "RPA") << "increasing " << alpha1 << std::endl;
+      } while (E1 <= E0);
+      LOG(1, "RPA") << "increased to " << alpha1 << std::endl;
+      // interval bisection
+      double alpha0(0.0);
+      while (alpha1-alpha0 > 1e-8) {
+        if (E0 < E1) {
+          alpha1 = alpham;
+          E1 = Em;
+        } else {
+          alpha0 = alpham;
+          E0 = Em;
+        }
+        alpham = 0.5*(alpha0+alpha1);
+        grid = g0;
+        auto dg(direction);
+        grid += dg *= alpham;
+        Em = getError(grid);
+        LOG(1, "RPA") << "bisecting " << alpha1 << std::endl;
+      }
+      LOG(1, "RPA") << "bisected to " << alpham << std::endl;
+      return Em;
+    }
+
+    void testGradient(const double stepSize) {
+      double E0(getError(grid));
+      FrequencyGrid grad(getGradient(grid));
+      FrequencyGrid numGrad(grid);
+      for (size_t n(0); n < grid.ws.size(); ++n) {
+        grid.ws[n] += stepSize;
+        double E1(getError(grid));
+        grid.ws[n] -= stepSize;
+        numGrad.ws[n] = (E1-E0) / stepSize;
+        LOG(1, "RPA") << "dE/dw[" << n << "] = " <<
+          grad.ws[n] << " ~ " << numGrad.ws[n] << std::endl;
+      }
+      for (size_t n(0); n < grid.ws.size(); ++n) {
+        grid.nus[n] += stepSize;
+        double E1(getError(grid));
+        grid.nus[n] -= stepSize;
+        numGrad.nus[n] = (E1-E0) / stepSize;
+        LOG(1, "RPA") << "dE/dnu[" << n << "] = " <<
+          grad.nus[n] << " ~ " << numGrad.nus[n] << std::endl;
       }
     }
 
@@ -56,97 +173,87 @@ namespace cc4s {
       return 4*delta*delta / std::pow(delta*delta + nu*nu, e);
     }
 
-    void optimize(const int stepCount, const double stepSize) {
-      NEW_FILE("L.dat");
-      for (int m(0); m < stepCount; ++m) {
-        // error of numerical quadrature with current grid wn[n] & nus[n] from
-        // analytic value 1/(eps_a-eps_i) for each ai
-        auto Eai(*Dai);
-        CTF::Transform<double>(
-          std::function<void(double &)>(
-            [this](double &delta) {
-              double error(0);
-              // use quadrature to integrate Mp2 propagator
-              for (size_t n(0); n < ws.size(); ++n) {
-                error += ws[n] * propagator(delta, nus[n]);
-              }
-              error /= Pi();
-              error -= 1 / delta;
-              delta = error;
+    double getError(const FrequencyGrid &grid) {
+      // error of numerical quadrature with current grid wn[n] & nus[n] from
+      // analytic value 1/(eps_a-eps_i) for each ai
+      CTF::Transform<double, double>(
+        std::function<void(double, double &)>(
+          [&grid](double delta, double &error) {
+            error = 0.0;
+            // use quadrature to integrate Mp2 propagator
+            for (size_t n(0); n < grid.ws.size(); ++n) {
+              error += grid.ws[n] * propagator(delta, grid.nus[n]);
+            }
+            error /= Pi();
+            // subtract analytic result = 1/delta
+            error -= 1 / delta;
+          }
+        )
+      ) (
+        (*Dai)["ai"], (*Eai)["ai"]
+      );
+
+      // get frobenius norm of errors, which is to be optimized
+      double E(Eai->norm2());
+      return 0.5*E*E;
+    }
+
+    // expects a call of getError with the same grid first
+    FrequencyGrid getGradient(const FrequencyGrid &grid) {
+      FrequencyGrid gradGrid(grid);
+      for (size_t n(0); n < grid.ws.size(); ++n) {
+        // derivative of 0.5*error(eps_a-eps_i)^2 with respect to n-th
+        // integration weight ws[n] for each ai
+        auto DWai(*Dai);
+        CTF::Transform<double, double>(
+          std::function<void(double, double &)>(
+            [&grid, n](double error, double &delta) {
+              double diff(error * propagator(delta, grid.nus[n]) / Pi());
+              delta = diff;
             }
           )
         ) (
-          Eai["ai"]
+          (*Eai)["ai"], DWai["ai"]
         );
+        // sum contributions from all energy gaps eps_a-eps_i
+        CTF::Scalar<double> DW;
+        DW[""] = DWai["ai"];
+        gradGrid.ws[n] = DW.get_val();
 
-        // get frobenius norm of errors, which is to be optimized
-        double L(Eai.norm2()*0.5);
-        FILE("L.dat") << m << " " << L;
-
-        for (size_t n(0); n < ws.size(); ++n) {
-          // derivative of 0.5*error(eps_a-eps_i)^2 with respect to n-th
-          // integration weight ws[n] for each ai
-          auto DWai(*Dai);
-          CTF::Transform<double, double>(
-            std::function<void(double, double &)>(
-              [this, n](double error, double &delta) {
-                double diff(error * propagator(delta, nus[n]));
-                delta = diff / Pi();
-              }
-            )
-          ) (
-            Eai["ai"], DWai["ai"]
-          );
-          // sum contributions from all energy gaps eps_a-eps_i
-          CTF::Scalar<double> DW;
-          DW[""] = DWai["ai"];
-          dws[n] = DW.get_val();
-
-          // derivative of 0.5*error(eps_a-eps_i)^2 with respect to n-th
-          // integration point nus[n] for each ai
-          auto DNuai(*Dai);
-          CTF::Transform<double, double>(
-            std::function<void(double, double &)>(
-              [this, n](double error, double &delta) {
-                double diff(
-                  error *
-                  (-2) * ws[n] * propagator(delta, nus[n], 3) *
-                  2*nus[n]
-                );
-                delta = diff / Pi();
-              }
-            )
-          ) (
-            Eai["ai"], DNuai["ai"]
-          );
-          // sum contributions from all energy gaps eps_a-eps_i
-          CTF::Scalar<double> DNu;
-          DNu[""] = DNuai["ai"];
-          dnus[n] = DNu.get_val();
-
-          // update grid
-          ws[n] -= stepSize * dws[n];
-          nus[n] -= stepSize * dnus[n];
-        }
-
-        for (size_t n(0); n < ws.size(); ++n) {
-          FILE("L.dat") << " " << ws[n];
-        }
-        for (size_t n(0); n < ws.size(); ++n) {
-          FILE("L.dat") << " " << nus[n];
-        }
-        FILE("L.dat") << std::endl;
+        // derivative of 0.5*error(eps_a-eps_i)^2 with respect to n-th
+        // integration point nus[n] for each ai
+        auto DNuai(*Dai);
+        CTF::Transform<double, double>(
+          std::function<void(double, double &)>(
+            [&grid, n](double error, double &delta) {
+              double diff(
+                error *
+                (-2) * grid.ws[n] * propagator(delta, grid.nus[n], 3) *
+                2*grid.nus[n]
+              );
+              delta = diff / Pi();
+            }
+          )
+        ) (
+          (*Eai)["ai"], DNuai["ai"]
+        );
+        // sum contributions from all energy gaps eps_a-eps_i
+        CTF::Scalar<double> DNu;
+        DNu[""] = DNuai["ai"];
+        gradGrid.nus[n] = DNu.get_val();
       }
+      return gradGrid;
     }
 
-    // current frequency integration grid
-    std::vector<double> ws;
-    std::vector<double> nus;
-    // gradient of sum_ai(0.5*error(eps_a-eps_i)^2) w.r.t. ws and nus
-    std::vector<double> dws;
-    std::vector<double> dnus;
+    FrequencyGrid grid;
 
+    // Dai = eps_a-eps_i for each ai
     CTF::Tensor<double> *Dai;
+
+    // error of numerical quadrature of current grid wn[n] & nus[n] from
+    // analytic value 1/(eps_a-eps_i) for each ai.
+    // The current grid is the argument of last getError(grid).
+    CTF::Tensor<double> *Eai;
   };
 }
 
