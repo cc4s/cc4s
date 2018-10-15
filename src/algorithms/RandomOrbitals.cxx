4c3ccde5861b7b47e17cce6cf5e75fa600e0b49d
Nikos Masios <nickmasios@gmail.com>
2018-10-15 21:06:42 +0200

    updated units in random orbitals

--- src/algorithms/RandomOrbitals.cxx
+++ src/algorithms/RandomOrbitals.cxx
@@ -25,37 +25,24 @@ void RandomOrbitals::run() {
     getTensorArgument<complex>("FullParticleHoleCoulombVertex")
   );
   GammaGai->set_name("GammaGai");
-  Tensor<complex> conjGammaGai(*GammaGai);
-  conjugate(conjGammaGai);
   Tensor<complex> *cbbeta(
     getTensorArgument<complex>("RandomOrbitalsCoefficients")
   );
-  int lens[] = { GammaGai->lens[0], cbbeta->lens[1], GammaGai->lens[2] };
-  int syms[] = { NS, NS, NS };
-  Tensor<complex> *GammaGalphai = new Tensor<complex>(
-    3, lens, syms, *GammaGai->wrld, "GammaGalphai"
-  );
-  allocatedTensorArgument<complex>(
-    "ParticleHoleCoulombVertex", GammaGalphai
-  );
-  LOG(1, "RandomOrbitals")
-    << "Using Nr=" << cbbeta->lens[1] << " random orbitals to build " << GammaGalphai->get_name()
-    << " from " << GammaGai->get_name() << " with Nv=" << GammaGai->lens[1] << " virtual orbitals" << std::endl;
-   // NOTE: cbbeta are not conjugated for transforming psi*_a(x) in GammaGai
-  (*GammaGalphai)["GAi"] = (*GammaGai)["Gai"] * (*cbbeta)["aA"];
-
-  Tensor<complex> conjGammaGalphai(*GammaGalphai);
-  conjugate(conjGammaGalphai);
+  Tensor<complex> conjGammaGai(*GammaGai);
+  conjugate(conjGammaGai);
 
   constexpr real eVPerHartree = 27.21138602;
   constexpr real angstromPerBohr = 0.52917721067;
-  real volume(getRealArgument("Volume"));
-  real gamma(getRealArgument("Gamma"));
+//  constexpr real kineticEV =
+//    eVPerHartree * pow(angstromPerBohr*2*Pi<>(),2);
+  // volume in a.u.
+  real volume(getRealArgument("Volume") / pow(angstromPerBohr,3));
+  real gamma(getRealArgument("Gamma") * 2*Pi<>() * angstromPerBohr);
   auto momenta(getTensorArgument<real>("Momenta"));
   // convert lengths of momenta into atomic units
   Transform<double>(
     std::function<void(double &)>(
-      [volume](real &momentum) {
+      [](real &momentum) {
         momentum *= 2*Pi<>() * angstromPerBohr;
       }
     )
@@ -63,46 +50,95 @@ void RandomOrbitals::run() {
     (*momenta)["G"]
   );
 
-  Tensor<complex> EG(1, lens, syms, *GammaGai->wrld, "EG");
-  // compute high energy approximation with full virtual orbitals
-  EG["G"] =
-    conjGammaGai["Gai"] * (*GammaGai)["Gbj"] *
-    conjGammaGai["Hai"] * (*GammaGai)["Hbj"];
-  // multiply with approximate energy denominator = G^2/2 + G^2/2 + gamma
-  // in ev
-  Transform<double, complex>(
+  // convert coulomb vertices into overlap densities
+  Tensor<complex> SGai(*GammaGai);
+  Tensor<complex> conjSGai(conjGammaGai);
+  Transform<double, complex> overlapDensityFromCoulombVertex(
     std::function<void(double, complex &)>(
-      [volume, gamma](real momentum, complex &e) {
-        e = e / (gamma + momentum*momentum) * eVPerHartree;
+      [volume](real momentum, complex &gamma) {
+        gamma /= sqrt(4*Pi<>() / pow(momentum,2) * eVPerHartree);
       }
     )
-  )(
-    (*momenta)["G"], EG["G"]
   );
-  // contract to approximate energy
-  Scalar<> E;
-  E[""] = EG["G"];
-  real randomEnergy(E.get_val());
+  overlapDensityFromCoulombVertex((*momenta)["G"], SGai["Gai"]);
+  overlapDensityFromCoulombVertex((*momenta)["G"], conjSGai["Gai"]);
+
+  int QLens[] = {SGai.lens[0], SGai.lens[0]};
+  int syms[] = { NS, NS, NS };
+  Tensor<complex> QGH(2, QLens, syms, *GammaGai->wrld, "QGH");
+  // compute high energy approximation with full virtual orbitals
+  QGH["GH"] = SGai["Gbj"] * SGai["Hbj"];
+  Tensor<complex> conjQGH(QGH);
+  conjQGH["GH"] = conjSGai["Gai"] * conjSGai["Hai"];
+  QGH["GH"] *= conjQGH["GH"];
+  Scalar<complex> E;
+  Transform<double, complex>(
+    std::function<void(double, complex &)>(
+      [volume](real momentum, complex &q) {
+        if (momentum > 0.0) {
+          q *= 4*Pi<>() / pow(momentum,2);
+        }
+      }
+    )
+  ) (
+    (*momenta)["H"], QGH["GH"]
+  );
+  Transform<double, complex>(
+    std::function<void(double, complex &)>(
+      [volume,gamma](real momentum, complex &q) {
+        if (momentum > 0.0) {
+          q *= 4*Pi<>() / pow(momentum,2) / (pow(gamma,2) + pow(momentum,2));
+        }
+      }
+    )
+  ) (
+    (*momenta)["G"], QGH["GH"]
+  );
+  E[""] = 2.0 * eVPerHartree * QGH["GH"];
+  complex randomEnergy(E.get_val());
   LOG(0,"RandOrb") << "using virtual orbitals MP2d ~ " << randomEnergy << std::endl;
 
+
   // again with random orbitals
-  EG["G"] =
-    conjGammaGalphai["GAi"] * (*GammaGalphai)["GBj"] *
-    conjGammaGalphai["HAi"] * (*GammaGalphai)["HBj"];
-  // multiply with approximate energy denominator = G^2/2 + G^2/2 + gamma
-  // in ev
+  int lens[] = { GammaGai->lens[0], cbbeta->lens[1], GammaGai->lens[2] };
+  Tensor<complex> SGalphai(
+    3, lens, syms, *GammaGai->wrld, "SGalphai"
+  );
+  LOG(1, "RandomOrbitals")
+    << "Using Nr=" << cbbeta->lens[1] << " random orbitals to build " << SGalphai.get_name()
+    << " from " << GammaGai->get_name() << " with Nv=" << GammaGai->lens[1] << " virtual orbitals" << std::endl;
+   // NOTE: cbbeta are not conjugated for transforming psi*_a(x) in GammaGai
+  SGalphai["GAi"] = SGai["Gai"] * (*cbbeta)["aA"];
+  Tensor<complex> conjSGalphai(SGalphai);
+  conjugate(conjSGalphai);
+
+  // compute high energy approximation with random orbitals
+  QGH["GH"] = SGalphai["Gbj"] * SGalphai["Hbj"];
+  conjQGH["GH"] = conjSGalphai["Gai"] * conjSGalphai["Hai"];
+  QGH["GH"] *= conjQGH["GH"];
   Transform<double, complex>(
     std::function<void(double, complex &)>(
-      [volume, gamma](real momentum, complex &e) {
-        e = e / (gamma + momentum*momentum) * eVPerHartree;
+      [volume](real momentum, complex &q) {
+        if (momentum > 0.0) {
+          q *= 4*Pi<>() / pow(momentum,2);
+        }
       }
     )
-  )(
-    (*momenta)["G"], EG["G"]
+  ) (
+    (*momenta)["H"], QGH["GH"]
   );
-
-  // contract to approximate energy
-  E[""] = EG["G"];
+  Transform<double, complex>(
+    std::function<void(double, complex &)>(
+      [volume,gamma](real momentum, complex &q) {
+        if (momentum > 0.0) {
+          q *= 4*Pi<>() / pow(momentum,2) / (pow(gamma,2) + pow(momentum,2));
+        }
+      }
+    )
+  ) (
+    (*momenta)["G"], QGH["GH"]
+  );
+  E[""] = 2.0 * eVPerHartree * QGH["GH"];
   randomEnergy = E.get_val();
   LOG(0,"RandOrb") << "using random orbitals MP2d ~ " << randomEnergy << std::endl;
 }
