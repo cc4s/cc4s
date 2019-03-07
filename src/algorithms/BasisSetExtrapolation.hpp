ce5e0a65ac4eb3cba7c00bf0edb68ea7a3eaeb20
Andreas Irmler <andreas.irmler@tuwien.ac.at>
2019-03-07 14:28:27 +0100

    BasisSetExtrapolation only features QGG&fitting

--- src/algorithms/BasisSetExtrapolation.hpp
+++ src/algorithms/BasisSetExtrapolation.hpp
@@ -15,37 +15,15 @@ namespace cc4s {
     );
     virtual ~BasisSetExtrapolation();
     virtual void run();
-    virtual void dryRun();
   protected:
-    int NG;
-    int NR;
-    int No;
-    int Nv;
-    int Np;
-    int NF;
-    std::vector<real> structureFactor;
-    std::vector<real> pairCorrelationFunction;
-    std::vector<Vector<>> reciprocalGrid;
-    std::vector<Vector<>> realSpaceMesh;
-    std::vector<real> realSpaceBasisSetCompleteness;    
-    
-    void extrapolation(real Gmin, real Gmax, int itype);
-    void basisSetCompletenessFullFTODDUMP();
-    void basisSetCompletenessFTODDUMPIA();
-    real leastSquareFit(std::vector<real> fitabsG, std::vector<real> fitSF);
-    real simplestWindow(real Gmin, real Gmax, real G);
-    real integrateSimplestWindow( real Gmin, real Gmax);
-    void GtoRFourier();
-    void RtoGFourier();
-    void readReciprocalGridFromFile();
-    void constructRealSpaceMesh(int augfac,std::vector<Vector<>>& realSpaceMesh);
-    void fourierCompleteness();
-    void inverseConvolution();
-    void QGG(int iStart, int iEnd);
-    void dryQGG(int iStart, int iEnd);
-    void realCGi();
-    void fitF12(int type, int iter, real minG, real maxG);
-    void calculateNewSF(int type, real gamma, CTF::Tensor<> *coulombKernel, CTF::Tensor<> *newSF, CTF::Tensor<> *resNewSF);
+
+    static constexpr int iterations = 10;
+
+    void evaluateQGG(int slice);
+    void fitF12(int type, real minG, real maxG);
+    void calculateNewSF(
+      int type, real gamma, CTF::Tensor<> *coulombKernel, CTF::Tensor<> *newSF, CTF::Tensor<> *resNewSF
+    );
   };
 }
 
