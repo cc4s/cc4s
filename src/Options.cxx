d074f46dd10efc5b9fcd23aa024792804d043390
Alejandro Gallo <aamsgallo@gmail.com>
2020-03-12 17:11:39 +0100

    Flags: remove unused flags and unixify existing flags
    This is a breaking change
    TODO: Implement -h and --help

--- src/Options.cxx
+++ src/Options.cxx
@@ -7,42 +7,23 @@
 using namespace cc4s;
 
 Options::Options(int argumentCount, char **arguments) {
-  profile = DEFAULT_PROFILE;
-  storeV = DEFAULT_STORE_V;
-  stridedIo = DEFAULT_STRIDED_IO;
   file = "calculation.cc4s";
   logLevel = DEFAULT_LOG_LEVEL;
   logFile = "cc4s.log";
-  rank = DEFAULT_RANK;
-  accuracy = DEFAULT_ACCURACY;
+  yamlFile = "cc4s.yaml";
   dryRun = false;
   for (int i(0); i < argumentCount; ++i) {
     std::string argument(arguments[i]);
-    if (argument == "-file" || argument == "-i") {
+    if (argument == "-i") {
       file = arguments[++i];
-    } else if (argument == "-profile") { // TODO: remove?
-      std::stringstream stream(arguments[++i]);
-      stream >> profile;
-    } else if (argument == "-storeV") { // TODO: remove?
-      std::stringstream stream(arguments[++i]);
-      stream >> storeV;
-    } else if (argument == "-stridedIo") { // TODO: remove?
-      std::stringstream stream(arguments[++i]);
-      stream >> stridedIo;
-    } else if (argument == "-logLevel") {
+    } else if (argument == "--log-level") {
       std::stringstream stream(arguments[++i]);
       stream >> logLevel;
     } else if (argument == "--yaml") {
       yamlFile = arguments[++i];
-    } else if (argument == "-logFile" || argument == "-o") {
+    } else if (argument == "-o") {
       logFile = arguments[++i];
-    } else if (argument == "-rank") { // TODO: remove?
-      std::stringstream stream(arguments[++i]);
-      stream >> rank;
-    } else if (argument == "-accuracy") { // TODO: remove?
-      std::stringstream stream(arguments[++i]);
-      stream >> accuracy;
-    } else if (argument == "-dryRun") {
+    } else if (argument == "--dry-run") {
       dryRun = true;
     }
   }
--- src/Options.hpp
+++ src/Options.hpp
@@ -7,22 +7,11 @@
 namespace cc4s {
   struct Options {
 
-    bool profile, storeV, stridedIo;
     int logLevel;
-    std::string yamlFile;
-    std::string logFile;
-    int rank;
-    double accuracy;
-    std::string file;
+    std::string yamlFile, logFile, file;
     bool dryRun;
 
-    const int DEFAULT_NITER = 8, DEFAULT_NW = 100;
-    const bool DEFAULT_PROFILE = false;
-    const bool DEFAULT_STORE_V = false;
-    const bool DEFAULT_STRIDED_IO = false;
     const int DEFAULT_LOG_LEVEL = 1;
-    const int DEFAULT_RANK = 0;
-    const double DEFAULT_ACCURACY = 1e-10;
 
     Options(int argumentCount, char **arguments);
   };
