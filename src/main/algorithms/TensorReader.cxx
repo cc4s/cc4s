fc7ba9acbf62d34a97b709b98b8a4f15f372fca3
Felix Hummel <felix.hummel@tuwien.ac.at>
2020-04-15 18:41:55 +0200

    prepared tensor reading algorithm

--- src/main/algorithms/TensorReader.cxx
+++ src/main/algorithms/TensorReader.cxx
@@ -1,6 +1,7 @@
 #include <algorithms/TensorReader.hpp>
 
 #include <Cc4s.hpp>
+#include <Parser.hpp>
 #include <tcc/Tcc.hpp>
 #include <math/Real.hpp>
 #include <math/Complex.hpp>
@@ -15,7 +16,11 @@ ALGORITHM_REGISTRAR_DEFINITION(TensorReader);
  * \brief Reader for tensor data.
  */
 Ptr<MapNode> TensorReader::run(const Ptr<MapNode> &arguments) {
-  return New<MapNode>();
+  auto fileName(arguments->getValue<std::string>("fileName"));
+  auto tensor(Parser(fileName).parse()->map());
+  auto result(New<MapNode>());
+  result->get("tensor") = tensor;
+  return result;
 }
 
 
