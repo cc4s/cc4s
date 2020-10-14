f9eccfb7e8d9721043ffd638ee43a2c0bf3eb34a
Alejandro Gallo <aamsgallo@gmail.com>
2020-10-14 13:05:24 +0200

    Update checks for ccsd

--- test/algorithms/CcsdEnergyFromCoulombIntegrals/real/check.py
+++ test/algorithms/CcsdEnergyFromCoulombIntegrals/real/check.py
@@ -5,5 +5,5 @@ from testis import read_yaml
 out = read_yaml("cc4s.out")
 
 assert out["dryRun"] == 0, "We should not be doing dryRuns now"
-assert (out["steps"][0]["out"]["tensor"]["fermiEnergy"] == -4.4484944443468191,
-       "Fermi energy is not correct")
+assert out["steps"][0]["out"]["tensor"]["fermiEnergy"] == -4.4484944443468191,\
+       "Fermi energy is not correct"
