f3ab878e3bde9ca6a688ff288a1856e5443258fd
Alejandro Gallo <aamsgallo@gmail.com>
2020-10-14 16:11:43 +0200

    Remove remnants of submodules and add commit hash in deps' build paths

--- Cc4s.mk
+++ Cc4s.mk
@@ -3,13 +3,13 @@
 
 # Cyclops Tensor Framework ====================================================
 CTF_COMMIT     ?= 29885baf124a74e185073b29f4c021b2e9dc11ba
-CTF_BUILD_PATH ?= $(abspath lib/build/${CONFIG}/ctf)
+CTF_BUILD_PATH ?= $(abspath lib/build/${CONFIG}/ctf/$(CTF_COMMIT))
 CTF_LIB        ?= -L${CTF_BUILD_PATH}/lib -lctf
 CTF_INCLUDE    ?= -I${CTF_BUILD_PATH}/include
 
 # yaml-cpp ====================================================================
 YAML_COMMIT     ?= c9460110e072df84b7dee3eb651f2ec5df75fb18
-YAML_BUILD_PATH ?= $(abspath lib/build/${CONFIG}/yaml-cpp)
+YAML_BUILD_PATH ?= $(abspath lib/build/${CONFIG}/yaml-cpp/$(YAML_COMMIT))
 YAML_LIB        ?= -L${YAML_BUILD_PATH} -lyaml-cpp
 YAML_INCLUDE    ?= -I${YAML_BUILD_PATH}/include
 
