a962f5a4621f976e7288268422394e48a0987211
Alejandro Gallo <aamsgallo@gmail.com>
2021-12-07 15:41:15 +0100

    Implement sources in testis and document testis

--- test/testis.py
+++ test/testis.py
@@ -12,13 +12,26 @@ import urllib.request
 import json
 import functools
 import re
+import zipfile
+import tarfile
 
 
-def get_store_folder():
+def _get_root_folder():
+    """
+    Get root folder where the testis script is located,
+    this is important to locate the testis-store folder and
+    the souces.json file.
+    """
     f = __file__
     while os.path.islink(f):
         f = os.readlink(f)
-    return op.join(op.dirname(op.abspath(f)), "testis-store")
+    return op.dirname(op.abspath(f))
+
+
+def get_store_folder():
+    return op.join(_get_root_folder(), "testis-store")
+
+
 
 
 __version__ = "0.0.1"
@@ -26,9 +39,11 @@ __author__ = "Alejandro Gallo"
 __email__ = "aamsgallo@gmail.com"
 __license__ = "GPLv3"
 STORE_FOLDER = get_store_folder()
+DEFAULT_SOURCES_FILE =  os.path.join(_get_root_folder(), "sources.json")
 INFO_FILE_NAME = "test.json"
 DEFAULT_RUN_SCRIPT = "./run.py"
 DEFAULT_CHECK_SCRIPT = "./check.py"
+SOURCE_TYPES = "raw zip tar".split(" ")
 CLEAR = "\x1b[0m"
 GREEN = "\x1b[32m"
 RED = "\x1b[31m"
@@ -36,15 +51,82 @@ MAGENTA = "\x1b[35m"
 
 TestCase = namedtuple("TestCase", "tags name path outpath "
                                   "description run check resources")
-Resource = namedtuple("Resource", "url out hash")
+Resource = namedtuple("Resource", "uri out hash")
+Source = namedtuple("Source", "name uri type")
+
+
+class RawSource(Source):
+    def __repr__(self):
+        return self.uri
+
+
+class _ArchiveSource(Source):
+    def __init__(self, *args, **kwargs):
+        self.context_opener = None
+        self.get_main_name = None
+
+    def __repr__(self):
+        resource = make_resource(dict(uri=self.uri, out=self.name), {})
+        outdir = op.dirname(get_resource_storage(resource))
+        result_path = op.join(outdir, "result")
+        if op.exists(result_path):
+            return result_path
+        get_resource(resource)
+        _archive_file = get_resource_storage(resource)
+        with self.context_opener(_archive_file) as f:
+            main_name = self.get_main_name(f)
+            main_path = op.join(outdir, main_name)
+            logging.info("Extracting archive file %s to %s", f, main_path)
+            f.extractall(outdir)
+            logging.debug("%slinking%s %s => %s",
+                          MAGENTA, CLEAR, main_path, result_path)
+            os.symlink(main_path, result_path)
+            logging.debug("Removing %s", _archive_file)
+            os.unlink(_archive_file)
+        return result_path
+
+
+class ZipSource(_ArchiveSource):
+    def __init__(self, *args, **kwargs):
+        self.context_opener = zipfile.ZipFile
+        self.get_main_name = lambda f: f.namelist()[0]
+
+class TarSource(_ArchiveSource):
+    def __init__(self, *args, **kwargs):
+        self.context_opener = tarfile.open
+        self.get_main_name = lambda f: f.getnames()[0]
+
+def get_sources(filepath):
+    "Get dictionary containing sources"
+    assert isinstance(filepath, str)
+    result = dict()
+    d = dict()
+    with open(filepath) as f:
+        d = json.load(f)
+    for name, v in d.items():
+        t = v["type"]
+        v["name"] = name
+        assert t in SOURCE_TYPES, "Type of source not recognised"
+        o = None
+        if t == "raw":
+            o = RawSource(**v)
+        elif t == "zip":
+            o = ZipSource(**v)
+        elif t == "tar":
+            o = TarSource(**v)
+        result[name] = o
+    return result
 
 
 def get_tests_in_dir(folder):
+    assert isinstance(folder, str)
     return [p for p, _, _ in os.walk(folder)
               if op.exists(op.join(p, INFO_FILE_NAME))]
 
 
-def folder_to_test(folder, outname):
+def folder_to_test(folder, outname, sources):
+    assert isinstance(folder, str)
+    assert isinstance(outname, str)
     info = op.join(folder, INFO_FILE_NAME)
 
     with open(info) as f:
@@ -60,16 +142,19 @@ def folder_to_test(folder, outname):
                    , description=_data.get("description", "")
                    , run=_data.get("run", DEFAULT_RUN_SCRIPT)
                    , check=_data.get("check", DEFAULT_CHECK_SCRIPT)
-                   , resources=parse_resources(_data.get("resources", []))
+                   , resources=map(lambda r: make_resource(r, sources),
+                                   _data.get("resources", []))
                    )
 
 
-def parse_resources(raw_resources):
-    assert isinstance(raw_resources, list)
-    return [ Resource( url=r["url"]
-                     , out=r["out"]
-                     , hash=hash_string(r["url"])
-                     ) for r in raw_resources ]
+def make_resource(raw, sources):
+    assert isinstance(sources, dict)
+    assert isinstance(raw, dict)
+    r = Resource(uri=raw["uri"].format(**sources),
+                 out=raw["out"],
+                 hash=hash_string(raw["uri"]))
+    logging.debug("\trendered uri: %s", r.uri)
+    return r
 
 
 def hash_string(r):
@@ -81,20 +166,33 @@ def hash_string(r):
 
 def get_resource(r):
     assert isinstance(r, Resource)
-    out = op.join(STORE_FOLDER, r.hash, r.out)
-    if not op.exists(out):
-        os.makedirs(op.dirname(out), exist_ok=True)
+    out = get_resource_storage(r)
+    if op.exists(out):
+        return
+    src = r.uri
+    os.makedirs(op.dirname(out), exist_ok=True)
+    if op.exists(src):
+        logging.info("%slinking%s %s => %s",
+                     MAGENTA, CLEAR, src, op.relpath(out))
+        os.symlink(src, out)
+    else:
         logging.info("%sdownlo.%s %s => %s",
                      MAGENTA, CLEAR, r.out, op.relpath(out))
-        data = urllib.request.urlopen(r.url).read()
+        data = urllib.request.urlopen(src).read()
         with open(out, 'wb+') as f:
             f.write(data)
+
+
+def get_resource_storage(r):
+    "Return actual path where the resource is stored in the testis store"
+    assert isinstance(r, Resource)
+    return op.abspath(op.join(STORE_FOLDER, r.hash, r.out))
 
 
 def link_resource(r, basedir):
     assert isinstance(r, Resource)
     assert isinstance(basedir, str)
-    i = op.abspath(op.join(STORE_FOLDER, r.hash, r.out))
+    i = get_resource_storage(r)
     o = op.join(basedir, r.out)
     if op.exists(o):
         return
@@ -180,6 +278,9 @@ def tail(xs, n):
 
 
 def filter_tags(tests, tags):
+    assert isinstance(tests, list)
+    assert isinstance(tests[0], TestCase)
+    assert isinstance(tags, str)
     tags_code = tags
     tags_code = re.sub(r".and.", r"&", tags_code)
     tags_code = re.sub(r".or.", r"|", tags_code)
@@ -193,7 +294,7 @@ def filter_tags(tests, tags):
     return [t for t in tests if tags_lambda(t.tags)]
 
 
-def main():
+def _get_args():
     parser = argparse.ArgumentParser("testis", "A minimal test run")
     parser.add_argument("folders",
                         help="Test folder where to find tests",
@@ -217,22 +318,31 @@ def main():
                         help="Name for the results folder of the test",
                         default="test-results",
                         type=str)
-    parser.add_argument(
-        "--tail",
-        help="How many lines to see from output when error occurs",
-        type=int,
-        default=10)
-    args = parser.parse_args()
+    parser.add_argument("--tail",
+                        help="Lines to see in output when error occurs",
+                        type=int,
+                        default=10)
+    parser.add_argument("--sources",
+                        help="Sources file to use",
+                        type=str,
+                        default=DEFAULT_SOURCES_FILE)
+    return parser.parse_args()
 
+
+def main():
+    args = _get_args()
     os.makedirs(STORE_FOLDER, exist_ok=True)
 
     if args.d:
         logging.basicConfig(
             level=logging.DEBUG,
-                format="(%(relativeCreated)d) %(levelname)s:%(message)s")
+            format="(%(relativeCreated)d) %(levelname)s:%(message)s")
     else:
         logging.basicConfig(level=logging.INFO,
                             format="(%(relativeCreated)d) %(message)s")
+
+    logging.info("Getting sources from %s", args.sources)
+    sources = get_sources(args.sources)
 
     # get only folders
     args.folders = [f for f in args.folders if os.path.isdir(f)]
@@ -243,7 +353,9 @@ def main():
     logging.info("Finding tests")
     for f in args.folders:
         logging.debug("in %s", f)
-        tests.extend(map(functools.partial(folder_to_test, outname=args.name),
+        tests.extend(map(functools.partial(folder_to_test,
+                                           outname=args.name,
+                                           sources=sources),
                          get_tests_in_dir(f)))
 
     logging.info("Found %s test folders", len(tests))
