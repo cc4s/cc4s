80f678af47cce80b64cff29d986239392383ba9e
Alejandro Gallo <aamsgallo@gmail.com>
2021-12-09 19:53:26 +0100

    Add python dependencies to vsc4 module files

--- etc/env/vsc4/icc_impi-debug.sh
+++ etc/env/vsc4/icc_impi-debug.sh
@@ -3,3 +3,6 @@ module load intel-mpi/2019.8
 module load intel-mkl/2020.2
 module load cmake/3.15.1-intel-19.1.1.217-nkzxutj
 
+# for tests
+module load python/3.6.6-gcc-9.1.0-mkqdc62
+module load py-pyyaml/3.13-gcc-9.1.0-f4qcohf
