c00da1391becfb05412dc9896b8b8c616d8a3a44
Alejandro Gallo <aamsgallo@gmail.com>
2021-12-17 14:57:22 +0100

    Comment the dist configuration

--- etc/config/dist.mk
+++ etc/config/dist.mk
@@ -1,3 +1,7 @@
+# AGA: DO NOT REMOVE THIS FILE
+# THIS CONFIGURATION IS NEEDED IN ORDER TO CREATE THE
+# THE CANONICAL DISTRIBUTION FOR CC4S
+
 include Cc4s.mk
 include Extern.mk
 include etc/make/scalapack.mk
