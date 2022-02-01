3fe2c9ecdd0b780577ac1fa00eef4d9918ce3831
Alejandro Gallo <aamsgallo@gmail.com>
2022-02-01 16:38:19 +0100

    Add check file to ueg

---
+++ test/tests/ueg/rs1.0-7occ-26virt/check.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+from testis import read_yaml, compare_energies
+
+out = read_yaml("cc4s.out")
+
+assert out["dryRun"] == 0, "We should not be doing dryRuns now"
+
+compare_energies("correct.out.yaml", "cc4s.out", accuracy=1e-7)
