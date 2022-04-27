ac598c1336f18bb41872139781072d67da021172
Nikolaos Masios <nmasios23@gmail.com>
2022-04-27 17:26:27 +0200

    Add first implementation of CompleteRenormalized intermediates

--- Sources.mk
+++ Sources.mk
@@ -34,6 +34,7 @@ main/algorithms/FiniteSizeCorrection.cxx \
 main/algorithms/BasisSetCorrection.cxx \
 main/algorithms/UegVertexGenerator.cxx \
 main/algorithms/PerturbativeTriples.cxx \
+main/algorithms/CompleteRenormalized.cxx \
 
 ifeq ($(ATRIP), no)
 SRC_FILES += main/algorithms/perturbative-triples/src/atrip/Atrip.cxx
---
+++ src/main/algorithms/CompleteRenormalized.hpp
@@ -0,0 +1,30 @@
+#ifndef COMPLETE_RENORMALIZED_PIECUCH
+#define COMPLETE_RENORMALIZED_PIECUCH
+
+#include <map>
+#include <TensorSet.hpp>
+
+namespace cc4s {
+
+
+// F = Field Value, double, complex etc.
+// TE = TensorEngine (CTF)
+/*
+ * These equations are taken from
+ *
+ *     Efficient computer implementation of the renormalized coupled-cluster methods:
+ *     The R-CCSD[T], R-CCSD(T), CR-CCSD[T], and CR-CCSD(T) approaches
+ *
+ *     https://doi.org/10.1016/S0010-4655(02)00598-2
+ * 
+ */
+template <typename F, typename TE>
+std::shared_ptr<TensorSet<F, TE>>
+ getCompleteRenormalized(
+  std::shared_ptr<TensorSet<F, TE>> coulombIntegrals,
+  std::shared_ptr<TensorSet<F, TE>> amplitudes
+); 
+
+}
+
+#endif
